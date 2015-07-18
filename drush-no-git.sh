e78f086f30d3ca1463ce88abeecfd4b86f68b8d5
Theodoros Ploumis <me@theodorosploumis.com>
2015-07-18 10:52:08 +0300

    Update drush-no-git.sh

--- drush-no-git.sh
+++ drush-no-git.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 # IMPORTANT. You must have curl, wget and php phar extension installed
-if [ command -v php >/dev/null 2>&1 ] then
+if command -v php >/dev/null
+then
   echo "php is not installed. Abording." >&2;
   exit 1;
 fi
 
-if [ command -v curl >/dev/null 2>&1 ] then
+if command -v curl >/dev/null
+then
   echo "curl is not installed. Abording." >&2;
   exit 1;
 fi
 
-if [ command -v testing >/dev/null 2>&1 ] then
+if command -v testing >/dev/null
+then
   echo "testing is not installed. Abording." >&2;
   exit 1;
 fi
 
 # Use Bash as command line (if you like you can use your own CL too!)
-chsh -s $(which bash) $USER
+chsh -s "$(which bash)" "$USER" 
 
 # Enter Drush version (branch from Github)
 # Notice that you need Drush 7.x (branch master) to be able to work with Drupal 8.x
@@ -28,12 +31,12 @@ mkdir ~/bin
 curl -sS https://getcomposer.org/installer | php -- --install-dir=bin
 
 # Download the files with wget. Get version from user input
-wget https://github.com/drush-ops/drush/archive/${branch}.tar.gz --no-check-certificate
+wget https://github.com/drush-ops/drush/archive/"${branch}".tar.gz --no-check-certificate
 
 # Untar it and rename the folder to /drush
-tar -zxvf ${branch}.tar.gz
-rm ${branch}.tar.gz
-mv ~/drush-${branch} ~/drush
+tar -zxvf "${branch}".tar.gz
+rm "${branch}".tar.gz
+mv ~/drush-"${branch}" ~/drush
 
 # Make the drush folder executable by current USER
 chmod u+x ~/drush/drush
